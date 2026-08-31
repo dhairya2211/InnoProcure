7d17cbb501d029272eb23021c4dcfbd1d236f4d7
netra <netra4806@gmail.com>
2026-09-01 01:48:26 +0530

    feat: add login API

--- server/server.js
+++ server/server.js
@@ -17,6 +17,8 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", require("./routes/authRoutes"));
+
 app.get("/", (req, res) => {
     res.json({
         message: "InnoProcure Backend is running"
