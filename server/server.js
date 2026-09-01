b13a26488859167fc7e8ebc9a4e95ca4fc3f186f
Vaishnavi <vaishnavipatel0304@gmail.com>
2026-09-01 16:23:16 +0530

    Complete backend APIs

--- server/server.js
+++ server/server.js
@@ -23,6 +23,7 @@ app.use("/api/applications", require("./routes/applicationRoutes"));
 app.use("/api/pilots", require("./routes/pilotRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/startups", require("./routes/startupRoutes"));
+
 app.get("/", (req, res) => {
     res.json({
         message: "InnoProcure Backend is running"
